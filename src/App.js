2258eaaf64123b7b67ad264fbafd1d9efd2dd7d3
Neel Bauman <neel.bauman@example.com>
2023-09-08 09:44:56 +0900

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
+import axios from 'axios';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	var datas;
+
+	axios.get("http://localhost:8000/")
+	.then(
+		function (response) {
+			datas = response.data;
+			console.log(datas);
+		}
+	)
+	.catch(
+		function (error) {
+			console.log(error);
+		}
+	)
+	.finally(
+	);
+
+	const list = datas.map( data => <p>{data.url}</p> )
+
+	return (
+	    <div>
+		{list}
+	    </div>
+	);
 }
 
 export default App;
