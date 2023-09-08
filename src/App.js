14c6c11e919a217dd00aa5c710530c382aaba3f8
Neel Bauman <neel.bauman@example.com>
2023-09-08 12:40:14 +0900

    WIP:showing site.url is working

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import './App.css';
 
+import React from 'react';
 import axios from 'axios';
 
 
 function App() {
-	var datas;
-
-	axios.get("http://localhost:8000/")
-	.then(
-		function (response) {
-			datas = response.data;
-			console.log(datas);
-		}
-	)
-	.catch(
-		function (error) {
-			console.log(error);
-		}
-	)
-	.finally(
-	);
-
-	const list = datas.map( data => <p>{data.url}</p> )
+	const [ data, setData ] = React.useState();
+	const url = "http://localhost:8000";
+	
+	const GetData = () => {
+		axios.get(url)
+		.then( (res) => {
+			setData(res.data.map( (site) => <p>{site.url}</p> ));
+		});
+	};
 
 	return (
 	    <div>
-		{list}
+		{data ? data : <button onClick={GetData}>データ</button>}
 	    </div>
 	);
 }
