9a4c5faeac76bacae7a502315a1086eeeb99e3d7
Neel Bauman <neel.bauman@example.com>
2023-09-08 18:29:58 +0900

    WIP:react-three/fiber is working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import './App.css';
 
-import React from 'react';
 import axios from 'axios';
+import { createRoot } from 'react-dom/client';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 
+function Box(props) {
+	const meshRef = useRef();
+	const [ hovered, setHover ] = useState(false);
+	const [ active, setActive ] = useState(false);
+
+	useFrame( (state, delta) => (meshRef.current.rotation.x += delta) );
+	
+	return (
+		<mesh
+			{...props}
+			ref = {meshRef}
+			scale = {active ? 1.5 : 1}
+			onClick = { (event) => setActive(!active) }
+			onPointerOver = { (event) => setHover(true) }
+			onPointerOut = { (event) => setHover(false) }>
+			<boxGeometry args={[1,1,1]} />
+			<meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+		</mesh>
+	);
+}
 
 function App() {
 	const [ data, setData ] = React.useState();
@@ -16,9 +38,15 @@ function App() {
 	};
 
 	return (
-	    <div>
-		{data ? data : <button onClick={GetData}>データ</button>}
-	    </div>
+		<div>
+			{data ? data : <button onClick={GetData}>データ</button>}
+			<Canvas>
+				<ambientLight/>
+				<pointLight position={[10,10,10]} />
+				<Box position={[-1.2,0,0]}/>
+				<Box position={[1.2,0,0]}/>
+			</Canvas>
+		</div>
 	);
 }
 
